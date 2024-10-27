e6d0f2a40f97005211701b0aa2d9f2ec5f8cf574
xricoma <xricoma@gmail.com>
2024-10-27 02:05:35 +0100

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
-resource "null_resource" "default" {
-  provisioner "local-exec" {
-    command = "echo 'Hello World, now with more characters'"
-  }
+provider "proxmox" {
+ pm_api_url   = "https://192.168.1.70:8006/api2/json"
+ pm_user      = "tf_user@pam!terraform"
+ pm_password  = "563176d7-9ba2-4ec2-872d-1e40c8fa0e6e"
+ pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "my_vm" {
+ name       = "NETALERTX2"
+ target_node = "kamel-server"
+ clone      = "NETALERTX"
+ storage    = "local-lvm"
+ cores      = 2
+ memory     = 2048
 }
